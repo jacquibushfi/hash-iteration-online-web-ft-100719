81cc1c5d69bbe5b455ff1e3c83e4db819838e871
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-10 17:34:14 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,7 @@ def age_appropriate_birthday(birthday_kids)
     if age <= 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     else
-      "You are too old for this"
+      "#{kids_name} You are too old for this."
     end
     
   end
